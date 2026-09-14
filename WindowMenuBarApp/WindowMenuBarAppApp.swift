3888357774261469f9836c1a40343d10374eb26e
Stephan <Stephan.Goergens@bmw.de>
2026-09-14 13:03:39 +0200

    Initial Commit

---
+++ WindowMenuBarApp/WindowMenuBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowMenuBarAppApp.swift
+//  WindowMenuBarApp
+//
+//  Created by Stephan Goergens on 14.09.26.
+//
+
+import SwiftUI
+
+@main
+struct WindowMenuBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
